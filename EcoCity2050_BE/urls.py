fb09aaad7b69455d582c52dd0c64ee761d4c19d9
kseohyuz <seohyun518612@naver.com>
2025-08-30 00:43:19 +0900

    feat: Swagger API 문서화 추가 및 JWT 인증 엔드포인트 문서 반영

--- EcoCity2050_BE/urls.py
+++ EcoCity2050_BE/urls.py
@@ -16,13 +16,31 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from users.views import UserDetailView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="EcoCity2050 API",
+      default_version='v1',
+      description="EcoCity2050 프로젝트 API 문서",
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # 로그인
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/user/', UserDetailView.as_view(), name='user_detail'),  # 로그인 후 사용자 정보
+
+# Swagger
+    path("swagger(<format>\.json|\.yaml)", schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path("swagger/", schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path("redoc/", schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
